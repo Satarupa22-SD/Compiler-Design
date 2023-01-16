c8fb8a29577afc0dec3b695fff9326a70081094e
Satarupa Deb <84276219+Satarupa22-SD@users.noreply.github.com>
2023-01-16 09:59:59 +0530

    Create oper.c

---
+++ oper.c
@@ -0,0 +1,20 @@
+( open para
+) close para
+{ openbrace
+} closebrace
+< lesser
+> greater
+" doublequote ' singlequote
+: colon
+; semicolon
+# pre processor
+== equal
+= asign
+% modulus
+^ bitwise
+& reference
+* star
++ add
+- sub
+\ backslash
+/ slash
